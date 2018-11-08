fd96152d051c8edefd1054468bb1f571a4cd995a
Cesar Bernal <cesarbworkmail@gmail.com>
2018-11-08 09:50:16 +0000

    simplifying the code and functionality

--- run.py
+++ run.py
@@ -6,16 +6,27 @@ app = Flask(__name__)
 messages = []
 
 
+def write_to_file(filename, data):
+    """Handle the writing to text file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
+
+
+
 def add_messages(username, message):
-    """Add messages to a mesage list"""
-    now = datetime.now().strftime("%H:%M:%S")
-    message_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(message_dict)
+    """Add messages to a text file"""
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"), 
+            username.title(), 
+            message))
     
     
 
 def get_all_messages():
     """get all messages and separate then with <br>"""
+    message = []
+    with open("data/messages.txt","r") as chat_messages:
+        messages = chat_messages.readlines()
     return messages
     
     
@@ -23,17 +34,20 @@ def get_all_messages():
 @app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
+    # handle post request
     if request.method == "POST":
-        with open("data/users.txt", "a") as user_list:
-            user_list.writelines(request.form['username'] + '\n')
+        write_to_file("data/users.txt", request.form['username'] + '\n')
         return redirect(request.form["username"])
     return render_template("index.html")
     
 
 
-@app.route('/<username>')
+@app.route('/<username>', methods=["GET", "POST"])
 def user(username):
     '''display chat names'''
+    if request.method == "POST":
+        add_messages(username, request.form["message"]+"\n")
+    
     messages = get_all_messages()
     return render_template("chat.html", username=username, chat_messages=messages)
     
